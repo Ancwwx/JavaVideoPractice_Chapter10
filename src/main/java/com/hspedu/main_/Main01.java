8f9835c3efcdd6e0fd7dc880d79ac33c1531c087
Ancwwx <ancwwx@gmail.com>
2023-03-12 21:53:55 +0900

    main,static

---
+++ src/main/java/com/hspedu/main_/Main01.java
@@ -0,0 +1,24 @@
+package com.hspedu.main_;
+
+public class Main01 {
+
+	private static String name = "hspedu";
+	private int n1 = 10000;
+
+	public static void hi() {
+		System.out.println("Main01的hi方法");
+	}
+
+	public void cry() {
+		System.out.println("Main01的 ary方法");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("name=" + name);
+		hi();
+		Main01 main01 = new Main01();
+		System.out.println(main01.n1);
+		main01.cry();
+
+	}
+}
---
+++ src/main/java/com/hspedu/static_/StaticMethodDetail.java
@@ -0,0 +1,18 @@
+package com.hspedu.static_;
+
+public class StaticMethodDetail {
+	public static void main(String[] args) {
+		D.hi();
+
+	}
+}
+
+class D {
+	public void say() {
+
+	}
+
+	public static void hi() {
+
+	}
+}
